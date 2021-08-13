cd40e2d13851abcd46a7b345fef042e7bd30eed3
Sonia Mara De Sá <soniamarasa@hotmail.com>
2021-08-13 14:43:56 -0300

    Chamando as APIs de Commerce

---
+++ service-example/node/middlewares/status.ts
@@ -0,0 +1,12 @@
+export async function status(ctx: Context, next: () => Promise<any>) {
+  const {
+    state: { code },
+    clients: { catalog },
+  } = ctx
+
+  const data = await catalog.getSkuById(code.toString())
+
+  ctx.body = data
+
+  await next()
+}
